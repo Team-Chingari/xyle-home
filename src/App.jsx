4001d76c3a05e30b704a3b3f64f39f455b662b6f
VidKothari <vidit.kothari@rediffmail.com>
2024-06-07 14:22:35 +0530

    added brands and odds sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css';
+import div_3_1_img from "./assets/div-3-1-img.png";
+import div_3_2_img from "./assets/div-3-2-img.png";
+
 
 export default function App() {
 
@@ -20,14 +23,38 @@ export default function App() {
       </div>
     </div>
 
-    <div className="div-3 relative bg-cover bg-center min-h-screen flex flex-col items-center justify-center">
+    <div className="div-3 relative bg-cover bg-center min-h-screen flex flex-col items-center justify-center space-y-20">
       <div className="text-center text-white font-gelasio font-bold text-8xl mx-40">
         Our Brands
       </div>
       <div className="flex space-x-10">
-          <img src="/assets/brand1.png" alt="Brand 1" className="w-1/3" />
-          <img src="/assets/div-3-2-img.png" alt="Brand 2" className="w-1/3" />
+          <img src={div_3_1_img} alt="Brand 1" className="w-100 h-100" />
+          <img src={div_3_2_img} alt="Brand 2" className="w-100 h-100" />
         </div>
+    </div>
+
+    <div className="div-4 relative bg-cover bg-center min-h-screen flex flex-col items-center justify-center text-center">
+      <div className="text-center text-white font-arial text-8xl mx-40 my-20">
+          <span className="block font-bold font-gelasio text-9xl">Feel The Odds Fall</span>
+          <span className="block font-bold font-gelasio text-9xl">In Your Favour</span>
+      </div>
+      <div className='div-4-2 h-100 flex justify-center space-x-60 text-center text-white px-96'>
+        <div className='py-4'>
+          <span className="block font-bold font-gelasio text-6xl px-5 p-2">150+</span>
+          <span className="block font-arial text-3xl">Clients</span>
+        </div>
+
+        <div className='py-4'>
+          <span className="block font-bold font-gelasio text-6xl px-5 p-2">320+</span>
+          <span className="block font-arial text-3xl">Websites/Apps</span>
+        </div>
+
+        <div className='py-4'>
+          <span className="block font-bold font-gelasio text-6xl px-5 p-2">95%</span>
+          <span className="block font-arial text-3xl">Customer Retention</span>
+        </div>
+        
+      </div>
     </div>
 
     </>
